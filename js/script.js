ac388be8a9413af9ae58be20b40563b4315c43bc
João Neto <jflorencioadv@gmail.com>
2023-12-16 15:14:08 +0100

    chuckListJoke

--- js/script.js
+++ js/script.js
@@ -1,46 +1,61 @@
-document.addEventListener('DOMContentLoaded', cargarChistes);
+const jokeList = document.getElementById('jokeList')
+const fetchJoke = document.getElementById('fetchJoke')
 
-document.getElementById('obtener-chiste').addEventListener('click', obtenerChiste);
-document.getElementById('eliminar-todos').addEventListener('click', eliminarTodos);
-
-function obtenerChiste() {
-    fetch('https://api.chucknorris.io/jokes/random')
-        .then(response => response.json())
-        .then(data => agregarChiste(data.value));
+// Función para obtener un chiste de Chuck Norris desde la API
+function fetchChuckNorrisJoke() {
+  return fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(data => data.value);
 }
 
-function agregarChiste(chiste) {
-    const lista = document.getElementById('lista-chistes');
-    const elemento = document.createElement('li');
-    elemento.innerHTML = `${chiste} <button onclick="eliminarChiste(this)">Eliminar</button>`;
-    lista.appendChild(elemento);
+// Función para renderizar la lista de chistes en el DOM
+function renderJokeList(jokes) {
+  jokeList.innerHTML = '';
 
-    guardarChistes();
+  jokes.forEach((joke, index) => {
+    
+    let itemsJoke = `
+      <li>
+        <p>${joke}</p>
+        <button class="deleteBtn" data-index="${index}">Eliminar</button>
+      </li>
+    `
+    jokeList.innerHTML += itemsJoke 
+  });
 }
 
-function cargarChistes() {
-    const chistes = JSON.parse(localStorage.getItem('chistes')) || [];
-    chistes.forEach(chiste => {
-        const lista = document.getElementById('lista-chistes');
-        const elemento = document.createElement('li');
-        elemento.innerHTML = `${chiste} <button onclick="eliminarChiste(this)">Eliminar</button>`;
-        lista.appendChild(elemento);
-    });
+// Función para guardar la lista de chistes en localStorage
+function saveJokesToLocalStorage(jokes) {
+  localStorage.setItem('chuckNorrisJokes', JSON.stringify(jokes));
 }
 
-function guardarChistes() {
-    const chistes = Array.from(document.querySelectorAll('#lista-chistes li'))
-                         .map(li => li.textContent.replace('Eliminar', '').trim());
-    localStorage.setItem('chistes', JSON.stringify(chistes));
+// Función para cargar la lista de chistes desde localStorage
+function loadJokesFromLocalStorage() {
+  const savedJokes = localStorage.getItem('chuckNorrisJokes');
+  console.log(JSON.parse(savedJokes))
+  return savedJokes ? JSON.parse(savedJokes) : [];
 }
 
-function eliminarChiste(boton) {
-    const li = boton.parentNode;
-    li.parentNode.removeChild(li);
-    guardarChistes();
-}
 
-function eliminarTodos() {
-    localStorage.clear();
-    document.getElementById('lista-chistes').innerHTML = '';
-}
+// Manejador de clic en el botón "Obtener Chiste"
+fetchJoke.addEventListener('click', () => {
+  fetchChuckNorrisJoke().then(joke => {
+    const jokes = [...loadJokesFromLocalStorage(), joke];
+    renderJokeList(jokes);
+    saveJokesToLocalStorage(jokes);
+  });
+});
+
+// Manejador de clic en los botones de eliminación
+jokeList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('deleteBtn')) {
+    const indexToDelete = event.target.getAttribute('data-index');
+    const jokes = loadJokesFromLocalStorage();
+    jokes.splice(indexToDelete, 1);
+    renderJokeList(jokes);
+    saveJokesToLocalStorage(jokes);
+  }
+});
+
+// Cargar chistes almacenados al cargar la página
+renderJokeList(loadJokesFromLocalStorage());
